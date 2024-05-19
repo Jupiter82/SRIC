fe933c190a0ee2f74ccad1ce9fd5ccae42bbdb7d
Jupiter Bade <jupiterbade@gmail.com>
2024-05-19 16:46:06 +0545

    4:45PM office push

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const http = require('http')
-const app = require("./src/config/express.config")
+const http = require("http");
+const app = require("./src/config/express.config");
 // const cors = require('cors')
 
-const httpServer = http.createServer(app)
-const port = 3005
+const httpServer = http.createServer(app);
+const port = 3005;
 
 httpServer.listen(port, (error) => {
-    if(!error){
-        console.log(`click here : http://localhost:${port}`)
-    }
-})
+  if (!error) {
+    console.log(`click here : http://localhost:${port}`);
+  }
+});
