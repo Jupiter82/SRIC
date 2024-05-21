b860e3ad558e55a6fefa10a1361ab8e23e6ac28d
Jupiter82 <jupiterbade@gmail.com>
2024-05-21 10:55:31 +0545

    simple push

--- src/modules/banner/banner.controller.js
+++ src/modules/banner/banner.controller.js
@@ -54,88 +54,89 @@ class BannerController {
       next(exception);
     }
   };
-  getBannerDetail = async (req,res,next) => {
-    try{
-        const data = await bannerSvc.getOneByFilter({_id: req.params.id})
-        if(!data){
-            throw{code: 404, message:"Banner does not exists"}
-        }else{
-            res.json({
-                result: data,
-                message: "Banner Fetched",
-                meta:null
-            })
-        }
-    }catch(exception){
-        next(exception)
-    }
-  }
-  updateById = async (req,res,next) => {
+  getBannerDetail = async (req, res, next) => {
     try {
-        const bannerDetail = await bannerSvc.getOneByFilter({_id: req.params.id});
-        if(!bannerDetail){
-            throw {code: 404, message: "Banner not found"}
-        }
-        const data = bannerSvc.transformRequest(req, true);
-        if(!data.image){
-            data.image = bannerDetail.image
-        }
-        
-        const success = await bannerSvc.updateBanner(req.params.id,data);
-        if(!success) {
-            throw {code:400, message: "Problem while updating banner"}
-        }
-        res.json({
-          result: success,
-          message: "Banner Updated successfully",
-          meta: null,
-        });
-      } catch (exception) {
-        console.log("BannerUpdate", exception);
-        next(exception);
-      }
-  }
-  deleteById = async (req, res, next) =>{
-    try {
-        let response = await bannerSvc.deleteById(req.params.id)
-        if(!response){
-            throw {code:400, message: "Problem while deleting banner"}
-        }
-        else{
-        res.json({
-            result:response,
-            message: "Banner Deleted successfully",
-            meta: null
-        })
-    }
-    } catch (exception) {
-        console.log("deleteById",exception);
-        next(exception)
-    }
-  }
-
-  listForHome = async (req, res, next) =>{
-    try {
-        const data = await bannerSvc.getAllBanners({
-          limit: 10,
-          skip: 0,
-          filter: {
-            status: "active"
-          }
-        });
-        if(!data || data.length <= 0){
-            throw{code:400, message:"Empty banner list"}
-        }
+      const data = await bannerSvc.getOneByFilter({ _id: req.params.id });
+      if (!data) {
+        throw { code: 404, message: "Banner does not exists" };
+      } else {
         res.json({
           result: data,
           message: "Banner Fetched",
-          meta: null
+          meta: null,
         });
-      } catch (exception) {
-        console.log(exception);
-        next(exception);
       }
-  }
+    } catch (exception) {
+      next(exception);
+    }
+  };
+  updateById = async (req, res, next) => {
+    try {
+      const bannerDetail = await bannerSvc.getOneByFilter({
+        _id: req.params.id,
+      });
+      if (!bannerDetail) {
+        throw { code: 404, message: "Banner not found" };
+      }
+      const data = bannerSvc.transformRequest(req, true);
+      if (!data.image) {
+        data.image = bannerDetail.image;
+      }
+
+      const success = await bannerSvc.updateBanner(req.params.id, data);
+      if (!success) {
+        throw { code: 400, message: "Problem while updating banner" };
+      }
+      res.json({
+        result: success,
+        message: "Banner Updated successfully",
+        meta: null,
+      });
+    } catch (exception) {
+      console.log("BannerUpdate", exception);
+      next(exception);
+    }
+  };
+  deleteById = async (req, res, next) => {
+    try {
+      let response = await bannerSvc.deleteById(req.params.id);
+      if (!response) {
+        throw { code: 400, message: "Problem while deleting banner" };
+      } else {
+        res.json({
+          result: response,
+          message: "Banner Deleted successfully",
+          meta: null,
+        });
+      }
+    } catch (exception) {
+      console.log("deleteById", exception);
+      next(exception);
+    }
+  };
+
+  listForHome = async (req, res, next) => {
+    try {
+      const data = await bannerSvc.getAllBanners({
+        limit: 10,
+        skip: 0,
+        filter: {
+          status: "active",
+        },
+      });
+      if (!data || data.length <= 0) {
+        throw { code: 400, message: "Empty banner list" };
+      }
+      res.json({
+        result: data,
+        message: "Banner Fetched",
+        meta: null,
+      });
+    } catch (exception) {
+      console.log(exception);
+      next(exception);
+    }
+  };
 }
 
 const bannerCtrl = new BannerController();
